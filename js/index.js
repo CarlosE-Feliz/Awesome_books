a3c92c599b4f553af012879f0e469110f6d34bd0
rotimiazeez <harzeyzhrotimi@gmail.com>
2021-08-10 23:32:59 +0100

    feat: Fix remove function to remove books from page

--- js/index.js
+++ js/index.js
@@ -1,54 +1,58 @@
-const addBtn = document.querySelector('.add');
-const bookTitle = document.getElementById('title');
-const bookAuthor = document.getElementById('author');
-
-const bookListWrap = document.querySelector('.book-ul');
-
-let bookCollection = {
-  books: [],
-};
-
-// GET BOOK COLLECTION FROM LOCALSTORAGE
-function getBooksFromLocalStorage() {
-  if (JSON.parse(localStorage.getItem('bookCollection'))) {
-    bookCollection = JSON.parse(localStorage.getItem('bookCollection'));
-    for (let i = 0; i < bookCollection.books.length; i += 1) {
-      const li = document.createElement('li');
-      li.innerHTML = '<div class="book-info">'
-            + '<p>'
-            + `<span class="book-title">${bookCollection.books[i].title}</span> by `
-            + `<span class="book-author">${bookCollection.books[i].author}</span>`
-            + '</p>'
-            + '</div>'
-            + '<button type="button" id="removeButton" class="delete">Remove</button>';
-      bookListWrap.appendChild(li);
+document.addEventListener('DOMContentLoaded', () => {
+  const addBtn = document.querySelector('.add');
+  const bookTitle = document.getElementById('title');
+  const bookAuthor = document.getElementById('author');
+  const bookListWrap = document.querySelector('.book-ul');
+  let bookCollection = {
+    books: [],
+  };
+  // GET BOOK COLLECTION FROM LOCALSTORAGE
+  function getBooksFromLocalStorage() {
+    if (JSON.parse(localStorage.getItem('bookCollection'))) {
+      bookCollection = JSON.parse(localStorage.getItem('bookCollection'));
     }
   }
-}
-getBooksFromLocalStorage();
-
-// ADD BOOK
-
-function updateLocalStorage() {
-  localStorage.setItem('bookCollection', JSON.stringify(bookCollection));
-}
-
-function addBook() {
-  bookCollection.books.push({ title: bookTitle.value, author: bookAuthor.value });
-  updateLocalStorage();
-}
-
-if (document.querySelector('.add')) {
-  addBtn.addEventListener('click', (e) => addBook(e));
-}
-
-// REMOVE BOOKS
-function deleteBook(el) {
-  if (el.classList.contains('delete')) {
-    el.parentElement.remove();
+  function updateLocalStorage() {
+    localStorage.setItem('bookCollection', JSON.stringify(bookCollection));
   }
-}
 
-document.querySelector('#list-books').addEventListener('click', (e) => {
-  deleteBook(e.target);
+  getBooksFromLocalStorage();
+  // IMPLEMENT BOOKS
+  function implementBooks() {
+    bookListWrap.innerHTML = '';
+    bookCollection.books.forEach((book, index) => {
+      bookListWrap.innerHTML += `<li>
+          <div class="book-info">
+            <p><span class="book-title">"${book.title}"</span> by <span class="book-author">${book.author}</span></p>
+          </div>
+          <button type="button" class="removeBtn" data-key=${index}>Remove</button>
+        </li>`;
+    });
+  }
+  if (document.querySelector('.book-ul')) {
+    implementBooks();
+  }
+  let removeBtn = [...document.querySelectorAll('.removeBtn')];
+  // ADD BOOK
+  function addBook(e) {
+    if (bookTitle.value.length <= 2 || bookAuthor.value.length <= 2) {
+      e.preventDefault();
+    } else {
+      bookCollection.books.push({ title: bookTitle.value, author: bookAuthor.value });
+      updateLocalStorage();
+    }
+  }
+  // Remove Book
+  const removeBook = (btn) => {
+    bookCollection.books = bookCollection.books.filter((book, i) => i !== Number(btn.dataset.key));
+    updateLocalStorage();
+    getBooksFromLocalStorage();
+    implementBooks();
+    removeBtn = [...document.querySelectorAll('.removeBtn')];
+    removeBtn.forEach((button) => button.addEventListener('click', () => removeBook(button)));
+  };
+  if (document.querySelector('.add')) {
+    addBtn.addEventListener('click', (e) => addBook(e));
+  }
+  removeBtn.forEach((button) => button.addEventListener('click', () => removeBook(button)));
 });
